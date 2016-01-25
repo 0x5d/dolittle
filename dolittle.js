c057c1c76912808932af0a7c79c2f10967e180fd
David Castillo <castillobgr@gmail.com>
2016-01-25 15:08:21 -0500

    Support for top-level arrays.

--- dolittle.js
+++ dolittle.js
@@ -21,17 +21,16 @@ angular.module('dolittle', [])
     if(typeof obj !== 'object'){
       return obj;
     }
-    else if(obj == null) {
+    if(obj == null) {
       return null;
+    }
+    if(obj instanceof Array) {
+        return translateArray(obj, convert);
     }
     var translatedObj = {};
     for(var prop in obj){
       if(obj[prop] instanceof Array){
-        var translatedArray = [];
-        for(var i = 0; i < obj[prop].length; i++){
-          translatedArray.push(iterate(obj[prop][i], convert));
-        }
-        translatedObj[convert(prop)] = translatedArray;
+        translatedObj[convert(prop)] = translateArray(obj[prop], convert);
       }
       else if((typeof obj[prop] !== null) && (typeof obj[prop] === 'object')){
         translatedObj[convert(prop)] = iterate(obj[prop], convert);
@@ -41,6 +40,14 @@ angular.module('dolittle', [])
       }
     }
     return translatedObj;
+  }
+
+  function translateArray(arr, convert) {
+    var translatedArray = [];
+    for(var i = 0; i < arr.length; i++){
+      translatedArray.push(iterate(arr[i], convert));
+    }
+    return translatedArray
   }
 
   var translate = {
